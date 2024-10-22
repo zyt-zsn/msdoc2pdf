68db928374f45f6146bbfafe2ffaf77c380a7206
zyt-zsn <zyt_xl@163.com>
2024-10-22 18:49:21 +0800

    Add password protection support.

--- msdoc2pdf.el
+++ msdoc2pdf.el
@@ -1,10 +1,38 @@
 (require 'cl)
+(require 'cl-macs)
+
 (add-to-list 'auto-mode-alist '("\\.\\(docx?\\|DOCX?\\|pptx?\\|PPTX?\\|xlsx?\\|XLSX?\\)\\'" . zyt/doc-mode))
 (defvar doc2pdf-convert-process nil)
 (defvar doc2pdf-convert-in-process nil)
-;; (defvar doc2pdf-arguments '("/bookmarks" "/password" "2023"))
-;; (defvar doc2pdf-arguments '("/password" "2023"))
-(defvar doc2pdf-arguments '("/password" "202"))
+(defvar doc2pdf-arguments '("/readonly" "/hidden"))
+
+(defcustom msdoc-to-pdf-program
+  (let ((executable (if (eq system-type 'windows-nt)
+                        "OfficeToPDF.exe" "OfficeToPDF"))
+        (default-directory
+         (or (and load-file-name
+                  (file-name-directory load-file-name))
+             default-directory)))
+    (cl-labels ((try-directory (directory)
+                  (and (file-directory-p directory)
+                       (file-executable-p (expand-file-name executable directory))
+                       (expand-file-name executable directory))))
+      (or (executable-find executable)
+          ;; This works if epdfinfo is in the same place as emacs and
+          ;; the editor was started with an absolute path, i.e. it is
+          ;; meant for Windows/Msys2.
+          (and (stringp (car-safe command-line-args))
+               (file-name-directory (car command-line-args))
+               (try-directory
+                (file-name-directory (car command-line-args))))
+          ;; If we are running directly from the git repo.
+          (try-directory (expand-file-name "../server"))
+          ;; Fall back to epdfinfo in the directory of this file.
+          (expand-file-name executable))))
+  "Filename of the msdoc2pdf executable."
+  :type 'file)
+
+
 (define-derived-mode zyt/doc-mode special-mode "ZYT Microsoft office document View"
   "View ms office files with pdf"
   (if doc2pdf-convert-in-process
@@ -26,13 +54,71 @@
 			;; (file-name-base doc-file)
 			;; ".pdf"))
 			)
-		(setq doc2pdf-convert-in-process t)
+		(message (format "Convert cmd:\n%s %s"
+						 msdoc-to-pdf-program
+						 (mapconcat
+						  'identity
+						  (append
+						   doc2pdf-arguments
+						   (list doc-file)
+						   (list pdf-file)
+						   )
+						  " "
+						  )))
+		(defun process-handler(process output)
+		  (setq doc2pdf-convert-in-process nil)
+		  (let ((exit-code (process-exit-status doc2pdf-convert-process)))
+			(if (member exit-code '(1 5))
+				(when-let ((password
+							(read-from-minibuffer
+							 (or (and (= 5 exit-code) "请输入文件密码: ")
+								 "密码错误，请重新输入:")
+							 nil nil nil  
+							 'minibuffer-history)))
+				  (setq doc2pdf-convert-process
+						(apply
+						 #'start-process
+						 "doc2pdf"
+						 (get-buffer-create "doc2pdf output")
+						 msdoc-to-pdf-program
+						 (append
+						  doc2pdf-arguments
+						  (list "/password" password)
+						  (list "/pdf_user_pass" password)
+						  (list doc-file)
+						  (list pdf-file)
+						  )
+						 )
+						)
+				  (set-process-sentinel doc2pdf-convert-process #'process-handler)					  
+				  (message "文件转换中，请稍候...")
+				  )
+			  (message (format "process %s code: %d\n" (process-status doc2pdf-convert-process) (process-exit-status doc2pdf-convert-process)))
+			  (if (null (string-prefix-p "finished" output))
+				  (progn
+					(message (format "转换失败：%s, 使用office打开相应文件" output))
+					;; (dired-visit-node-in-external-application)
+					)
+				(kill-buffer cur-buffer)
+				(message (format "转换结束:%s" output))
+				(find-file pdf-file)
+				;; (setf buffer-file-name (concat (file-name-directory doc-file)
+				;; (file-name-base doc-file)
+				;; ".pdf"))
+				)
+			  (with-current-buffer
+				  (process-buffer doc2pdf-convert-process)
+				(end-of-buffer)
+				(insert output)
+				)
+			  )
+			))
 		(setq doc2pdf-convert-process
 			  (apply
 			   #'start-process
 			   "doc2pdf"
 			   (get-buffer-create "doc2pdf output")
-			   "D:/Software/Editor/OfficeToPdf/OfficeToPDF/OfficeToPDF.exe"
+			   msdoc-to-pdf-program
 			   (append
 				doc2pdf-arguments
 				(list doc-file)
@@ -40,29 +126,8 @@
 				)
 			   )
 			  )
-		(set-process-sentinel doc2pdf-convert-process
-							  (lambda(process output)
-								(setq doc2pdf-convert-in-process nil)
-								(message (format "process %s code: %d\n" (process-status doc2pdf-convert-process) (process-exit-status doc2pdf-convert-process)))
-								(if (null (string-prefix-p "finished" output))
-									(progn
-									  (message (format "转换失败：%s, 使用office打开相应文件" output))
-									  ;; (dired-visit-node-in-external-application)
-									  )
-								  (kill-buffer cur-buffer)
-								  (message (format "转换结束:%s" output))
-								  (find-file pdf-file)
-								  ;; (setf buffer-file-name (concat (file-name-directory doc-file)
-								  ;; (file-name-base doc-file)
-								  ;; ".pdf"))
-								  )
-								(with-current-buffer
-									(process-buffer doc2pdf-convert-process)
-								  (end-of-buffer)
-								  (insert output)
-								  )
-								)
-							  )
+		(setq doc2pdf-convert-in-process t)
+		(set-process-sentinel doc2pdf-convert-process #'process-handler)					  
 		(message "文件转换中，请稍候...")
 		(kill-buffer cur-buffer)
 		(find-file directory)
